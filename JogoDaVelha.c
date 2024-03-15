c0bc59cf08712f55fbd1008097b4fb9b9d3582ae
AkiraNim <ismaeldejesus193@gmail.com>
2024-03-15 12:17:13 -0300

    mod

--- JogoDaVelha.c
+++ JogoDaVelha.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<windows.h>
 void linhaCol(int lin, int col){
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD),(col-1, lin-1));
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD) (col-1, lin-1));
 
 }
 int main (){
